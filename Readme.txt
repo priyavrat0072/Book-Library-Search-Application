3217db35c64f7777662fc28232b41f21af37dd26
priyavrat0072 <priyavrat0072@gmail.com>
2026-07-29 16:44:15 +0530

    Collecting the api's

---
+++ Readme.txt
@@ -0,0 +1,12 @@
+main API for Books and authors = https://openlibrary.org/search.json?q=${queryText}
+title, author_name, cover_i,first_publish_year,key
+
+for the image of the book = https://covers.openlibrary.org/b/id/15155833-M.jpg
+cover of the book
+
+For short description = https://openlibrary.org/works/OL27448W.json (key)
+description
+
+For fetching publisher details = https://openlibrary.org/works/OL27448W/editions.json (key + editons)
+publisher
+
--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const inputData = document.getElementById("inputbox")
-const searchBtn = document.getElementById("searchBtn")
 
-searchBtn.addEventListener("click",()=>{
-  console.log(inputData.value)
+
+const getBookData = async(queryText) =>{
+  let response = await fetch(`https://openlibrary.org/search.json?q=${queryText}`)
+  let data = await response.json()
+  console.log(data.docs)
+  // data.docs.map((item) =>{
+  //   console.log(item.title)
+  //   console.log(item.author_name)
+  //   console.log(item.cover_i)
+  //   console.log(item.first_publish_year)
+  //   console.log(item.key)
+  //   console.log(`==================================`)
+  // })
+  
+}
+
+const searchBtn = document.getElementById("searchBtn")
+searchBtn.addEventListener("click",async()=>{
+  queryText = inputData.value 
+  
+ await getBookData(queryText)
+
 })
